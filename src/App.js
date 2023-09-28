6bba5b2b9fa05f9c795751243ee2aa79ae1a90a2
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-09-29 08:48:48 +0900

    useStateの利用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+  function decrement() {
+    setCount(count - 1);
+  }
   return (
     <div className="tutorial">
-      <h1>useState Simply Explained!</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }

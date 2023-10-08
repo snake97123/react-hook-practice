d9840927a46a9afd75cb67b5d69da35090e22bd0
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-10-08 11:12:22 +0900

    コードのリファクタリング

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  function decrement() {
-    setCount(count - 1);
-  }
+
   return (
     <div className="tutorial">
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 }

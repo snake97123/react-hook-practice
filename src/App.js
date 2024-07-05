2e43fcd4b492a50ad936684571bd73956ea9b1fa
snake97123 <73522198+snake97123@users.noreply.github.com>
2024-07-06 08:57:29 +0900

    useEffectの練習

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
-function App() {
-  const [value, setValue] = useState(0);
-  
-  return (
-    <div>
-      <p>現在のカウント数：{value}</p>
-      {/* +1と-1とリセットボタンの設置 */}
-      <button className="plus" onClick={() => setValue(value + 1)}>+1</button>
-      <button className="minus" onClick={() => setValue(value - 1)}>-1</button>
-      <button className="reset" onClick={() => setValue(0)}>リセット</button>
-    </div>
-  )
-}
+const App = () => {
+   // 状態変数の初期値は０
+    const [count, setCount] = useState(0);
+
+   // 副作用関数の宣言
+   // countの値が変更された時に実行される
+   const callbackFunction = () => {
+     // titleの変更
+     document.title = `You clicked ${count} times`;
+   }
+
+   useEffect(callbackFunction, [count]);
+
+   const increment = () => {
+      setCount(count + 1);
+   }
+
+   const countReset = () => {
+      setCount(0);
+   };
+
+   return (
+      <div className="App">
+        <p>現在のカウント数：{count}</p>
+
+        <button onClick={increment}>カウントアップ</button>
+        <button onClick={countReset}>リセット</button>
+      </div>
+   );
+
+ };
 
 export default App;
